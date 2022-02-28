869c996b758d0ecaf0cc74e36c01dcb13d942b8e
Nicholas Boleky <hotfeet100@hotmail.com>
2022-02-27 19:21:31 -0800

    first commit

---
+++ Memory Game/Memory_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_GameApp.swift
+//  Memory Game
+//
+//  Created by Nicholas Boleky on 2/27/22.
+//
+
+import SwiftUI
+
+@main
+struct Memory_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
