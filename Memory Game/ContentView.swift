57cded20fe45ce1279c219d98085b81403a7e0f3
Nicholas Boleky <hotfeet100@hotmail.com>
2022-02-27 20:30:16 -0800

    Beginings of cards built out

--- Memory Game/ContentView.swift
+++ Memory Game/ContentView.swift
@@ -9,13 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
+        
     }
+    
+    struct CardView: View {
+        
+        @State var isFaceUp: Bool = false
+        let card = RoundedRectangle(cornerRadius: 20)
+        
+        
+        var body: some View {
+            ZStack {
+                if isFaceUp {
+                    card
+                        .fill()
+                        .foregroundColor(.white)
+                    card
+                        .stroke()
+                    Text("✈️")
+                        .font(.largeTitle)
+                } else {
+                    card
+                        .fill()
+                }
+            }
+            .onTapGesture {
+                isFaceUp.toggle()
+            }
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+.previewInterfaceOrientation(.portraitUpsideDown)
     }
 }
